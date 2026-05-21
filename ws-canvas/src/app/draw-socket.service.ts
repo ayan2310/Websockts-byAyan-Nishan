7fea54cb7edafbeb151cd33c07182e2524ed2712
ayan2310 <m.ayan@students.htl-leonding.ac.at>
2026-05-21 23:45:42 +0200

    draw-socket service

---
+++ ws-canvas/src/app/draw-socket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+export interface DrawData {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DrawSocketService {
+
+}
